8029d789bb98f40a2e39c348182867e6ed1ec7c1
shiping xia <ping9719@outlook.com>
2019-09-30 14:55:23 +0800

    ver 1

---
+++ DanMu/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DanMu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 308);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DanMu/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DanMu
+{
+    public partial class Form1 : Form
+    {
+        List<Label> TanMuList = new List<Label>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Rectangle rec = Screen.GetWorkingArea(this);
+            this.Height = rec.Height / 3 * 2; // 3分之2
+            this.Width = rec.Width;
+            this.Location = new Point(0, 0);
+            this.BackColor = Color.White;
+            this.TransparencyKey = Color.White;
+            this.Opacity = 1;
+
+            Thread th1 = new Thread(new ThreadStart(aaaa));
+            th1.Start();
+
+            AddTanMu("你开启了弹幕");
+        }
+
+        //测试
+        private void aaaa()
+        {
+            while (true)
+            {
+                AddTanMu(Guid.NewGuid().ToString());
+                Thread.Sleep(new Random().Next(1000, 3000));
+            }
+        }
+
+        private void AddTanMu(String word)
+        {
+            Label label = new Label();
+            label.Text = word;
+            label.AutoSize = true;
+            label.ForeColor = Color.Red;
+            label.BackColor = Color.Transparent;
+            label.Font = new Font("宋体", 20);
+            label.Location = new Point(this.Width, new Random().Next(this.Height - label.Height));
+
+            TanMuList.Add(label);
+
+            this.Invoke(new Action(() =>
+            {
+                this.Controls.Add(label);
+            }));
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //只能使用for循环
+            for (int i = 0; i < TanMuList.Count; i++)
+            {
+                Label label = TanMuList[i];
+
+                Point point = label.Location;
+
+                //步数、速度
+                for (int v = 0; v < 7; v++)
+                {
+                    point.X = point.X - 1;
+                    label.Location = point;
+                }
+
+                //超出移除
+                if (point.X + label.Width < 0)
+                {
+                    label.Visible = false;
+                    TanMuList.Remove(label);
+                    this.Controls.Remove(label);
+                }
+            }
+        }
+
+    }
+}
